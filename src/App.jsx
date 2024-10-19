cbe5b164f279f4946109528d08f5b9e0ac46ec64
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2024-10-19 15:58:42 +0330

    first part

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import ArrayComponent from "./components/ArrayComponent";
+import Debounce from "./components/Debounce";
+import Timeout from "./components/Timeout";
+import Toggle from "./components/Toggle";
+import UpdateEffect from "./components/UpdateEffect";
+
+function App() {
+  return (
+    <div>
+      {/* <Toggle defaultValue={true} /> */}
+      {/* <Timeout /> */}
+      {/* <Debounce /> */}
+      {/* <UpdateEffect /> */}
+      {/* <ArrayComponent /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ArrayComponent.jsx
@@ -0,0 +1,23 @@
+import useArray from "../hooks/useArray";
+
+function ArrayComponent() {
+  const { array, set, push, remove, filter, update, clear } = useArray([
+    1, 2, 3, 4, 5, 6,
+  ]);
+
+  return (
+    <div>
+      <div>{array.length ? array.join(", ") : "[ ]"}</div>
+      <button onClick={() => push(7)}>push 7</button>
+      <button onClick={() => update(1, 9)}>change second element to 9</button>
+      <button onClick={() => remove(1)}>remove second element</button>
+      <button onClick={() => filter((n) => n < 3)}>
+        keep nummbers less than 4
+      </button>
+      <button onClick={() => set([1, 2])}>set to 1,2</button>
+      <button onClick={clear}>Clear</button>
+    </div>
+  );
+}
+
+export default ArrayComponent;
